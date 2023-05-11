bf94126ad4bfaf01d84136b0003b505279b194c0
ouija98 <sweetenedlip@gmail.com>
2023-05-11 15:53:55 +0200

    Category API tests

---
+++ Shopster.Tests/APItests/Fixtures/ApiWebApplicationFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using Shopster.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shopster.Tests.APItests.Fixtures
+{
+    public class ApiWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(config =>
+            {
+                var integrationConfig = new ConfigurationBuilder()
+                  .AddJsonFile("integrationsettings.json")
+                  .Build();
+
+                config.AddConfiguration(integrationConfig);
+            });
+
+            // It is called after the `ConfigureServices` from the Startup.
+            builder.ConfigureTestServices(services =>
+            {
+                //services.AddTransient<IServerService, ServerServiceStub>();
+            });
+        }
+
+    }
+}
---
+++ Shopster.Tests/APItests/Fixtures/IntegrationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using CookBook.Api.Options;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using Xunit;
+
+namespace Shopster.Tests.APItests.Fixtures
+{
+    public class IntegrationTests : IClassFixture<ApiWebApplicationFactory>
+    {
+        protected readonly ApiWebApplicationFactory _factory;
+        protected readonly HttpClient _client;
+
+        //tady potrebuju vytvaret klienta ktereho budu pouzivat na provolavani dotazu
+        //do konstruktoru budu posilat moji fixture
+        public IntegrationTests(ApiWebApplicationFactory fixture) 
+        {
+            _factory = fixture;
+            //vytvoreni http klienta:
+            _client = _factory.CreateClient();
+        }
+    }
+}
